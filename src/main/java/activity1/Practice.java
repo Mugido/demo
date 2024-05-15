5445035c02def28161c9a057e5f9fabd80dfa419
Mugido <sirmugido707@gmail.com>
2024-05-15 15:33:44 +0100

    add files

---
+++ src/main/java/activity1/Practice.java
@@ -0,0 +1,10 @@
+package activity1;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.print("Hello, hello.\nI don't know why you say goodbye,\nI say hello.");
+
+        //System.out.println("I don't know why you say goodbye");
+        //System.out.println("I say hello");
+    }
+}
---
+++ src/main/java/local_variable/LocalVariable.java
@@ -0,0 +1,12 @@
+package local_variable;
+
+public class LocalVariable {
+
+       int num1 = 20; //instance variable
+
+    public void localVariable(){
+        int num = 10; //LOCAL VAriable
+
+        System.out.println(num + num1);
+    }
+}
